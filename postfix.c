0196d78be02a5b739e4331d439d0c4c06fdd19b3
cashiiyy <kasinathanps100@example.com>
2026-08-01 12:36:47 +0530

    Fixed evaluation of the postfix expression

--- postfix.c
+++ postfix.c
@@ -1,113 +1,140 @@
 #include <stdio.h>
-#define max 20
+#include <string.h>
+
+#define MAX 20
+
+int stack[MAX];
 int top = -1;
 
-int peek(char array[], int top) {
-    return array[top - 1];
-}
-
-int isFull(char array[], int top) {
-    if (top == max - 1) return 1;
-    else return 0;
-}
-
-int isEmpty(char array[], int top) {
-    if (top == -1) return 1;
-    else return 0;
-}
-
-
-char pop(char array[], int top1) {
-    if (top1 <= 0) {
-        return -1;
+void push(int c) {
+    if (top < MAX - 1) {
+        stack[++top] = c;
     }
-    char temp = array[top1 - 1];
-    return temp;
 }
 
+int pop() {
+    if (top >= 0) {
+        return stack[top--];
+    }
+    return -1;
+}
+
+int peek() {
+    if (top >= 0) {
+        return stack[top];
+    }
+    return -1;
+}
+
+// In-Stack Priority
 int ISP(char c) {
     if (c == '(') return 0;
     if (c == '+' || c == '-') return 2;
     if (c == '*' || c == '/') return 4;
     if (c == '^') return 5;
-    if (c != '+' && c != '-' && c != '*' && c != '/' && c != '^') return 8;
-    return -1;
+    return 0;
 }
 
+// Incoming Priority
 int ICP(char c) {
-    if (c == ')') return 0;
     if (c == '(') return 9;
     if (c == '+' || c == '-') return 1;
     if (c == '*' || c == '/') return 3;
     if (c == '^') return 6;
-    if (c != '+' && c != '-' && c != '*' && c != '/' && c != '^') return 7;
-    return -1;
+    return 0;
 }
 
+int isOperand(char c) {
+    return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z');
+}
+int eval(int x,int y,char s) {
+    if (s == '+') {
+        return (x+y);
+    }
+    if (s == '-') {
+        return (x-y);
+    }
+    if (s == '*') {
+        return (x*y);
+    }
+    if (s == '/') {
+        return (x/y);
+    }
+    if (s == '^') {
+        return (x^y);
+    }
+}
 int main() {
-    char st[max];
-    char item;
-    char x;
-    int i = 0;
-
-    char str[max] = "(a+b*c-d/e)";
-
-    top = 0;
-    st[top] = '(';
-    top++;
-    i++;
+    char str[MAX] = "(a+(b*c)/d)";
+    char post[MAX];
+    int i = 0, p = 0;
 
     printf("Infix Expression: %s\n", str);
-    printf("Postfix Expression: ");
 
-    while (top > 0) {
-        item = str[i];
+    while (str[i] != '\0') {
+        char item = str[i];
 
-        x = pop(st, top);
-        top--;
-
-        // Case 1: Operands (Letters)
-        if (item != '+' && item != '-' && item != '*' && item != '/' && item != '(' && item != ')' && item != '\0') {
-            // Restore stack state since we popped x early
-            st[top] = x;
-            top++;
-            printf("%c", item);
+        // Case 1: Operands
+        if (isOperand(item)) {
+            post[p++] = item;
             i++;
         }
-        // Case 2: Closing parenthesis
+        // Case 2: Opening Parenthesis
+        else if (item == '(') {
+            push(item);
+            i++;
+        }
+        // Case 3: Closing Parenthesis
         else if (item == ')') {
-            while (x != '(') {
-                printf("%c", x);
-                x = pop(st, top);
-                top--;
+            while (top != -1 && peek() != '(') {
+                post[p++] = pop();
             }
-            i++; // Move past ')'
-        }
-        // Case 3: Incoming operator has lower/equal priority -> Pop stack
-        else if (ISP(x) >= ICP(item)) {
-            while (ISP(x) >= ICP(item)) {
-                printf("%c", x);
-                x = pop(st, top);
-                top--;
-            }
-            st[top] = x;
-            top++;
-            st[top] = item;
-            top++;
+            pop(); // Remove '(' from stack
             i++;
         }
-        // Case 4: Incoming operator has higher priority -> Push to stack
-        else if (ISP(x) < ICP(item)) {
-            st[top] = x;
-            top++;
-            st[top] = item;
-            top++;
-            i++;
-        }
+        // Case 4: Operators
         else {
-            printf("INVALID EXPRESSION");
+            while (top != -1 && ISP(peek()) >= ICP(item)) {
+                post[p++] = pop();
+            }
+            push(item);
+            i++;
         }
     }
-    printf("\n");
+
+    // Pop remaining elements
+    while (top != -1) {
+        post[p++] = pop();
+    }
+
+    post[p] = '#'; // Null-terminate the string
+    post[p+1] = '\0';
+    printf("Postfix Expression: %s\n", post);
+
+    //evaluation
+    top=-1;
+    i=0;
+    int x;
+    int y;
+    char op;
+    int t;
+    char item=post[i];
+    while (item != '#') {
+        if (isOperand(item)) {
+            printf("\nENTER THE VALUE OF %c : ",item);
+            scanf(" %d",&x);
+            push(x);
+        }
+        else {
+            op=item;
+            y=pop();
+            x=pop();
+            t = eval(x,y,op);
+            push(t);
+        }
+        i++;
+        item=post[i];
+    }
+    printf("\nVALUE OF FINAL EXPRESSION : %d ",pop());
     return 0;
 }
